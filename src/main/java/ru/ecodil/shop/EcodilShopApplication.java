45e675b16165b5f7cb2e691e6add121c7e6a6009
RomanovV <aka.nonsens@gmail.com>
2024-04-16 14:13:28 +0300

    spring

---
+++ src/main/java/ru/ecodil/shop/EcodilShopApplication.java
@@ -0,0 +1,13 @@
+package ru.ecodil.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcodilShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcodilShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ecodil/shop/EcodilShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ecodil.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcodilShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
